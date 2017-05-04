6173bd854aa35a9506e6728d0bbafb9aa531bacf
maltaesousa <sos@ylb.ch>
2017-05-04 17:03:49 +0000

    Added database config for heroku

--- config/database.php
+++ config/database.php
@@ -38,6 +38,17 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
+        
+        'heroku' => [  
+            'driver'   => 'pgsql',
+            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ],
 
         'mysql' => [
             'driver' => 'mysql',
